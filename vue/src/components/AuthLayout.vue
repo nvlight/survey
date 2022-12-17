378138d2ae359e39877794f07032066bc999b384
nvlight <lightstyle11@mail.ru>
2022-12-17 12:12:56 +0300

    add AuthLayout, improve router for login/register

---
+++ vue/src/components/AuthLayout.vue
@@ -0,0 +1,21 @@
+<template>
+    <!--
+      This example requires updating your template:
+
+      ```
+      <html class="h-full bg-gray-50">
+      <body class="h-full">
+      ```
+    -->
+    <div class="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+        <div class="w-full max-w-md space-y-8">
+            <router-view></router-view>
+        </div>
+    </div>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+</style>
--- vue/src/store/index.js
+++ vue/src/store/index.js
@@ -1,9 +1,11 @@
 import {createStore} from "vuex";
 
 const store = createStore({
+    // change vite port
+    // npm run dev -- --port 5180
     state: {
         user:{
-            date: {name: 'Martin', age: 33,},
+            date: {},
             token: null,
         }
     },
