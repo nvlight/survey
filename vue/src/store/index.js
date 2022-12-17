036672963d96b808e6060a44e067d94fbb1faea4
nvlight <lightstyle11@mail.ru>
2022-12-17 16:59:57 +0300

    improved DefaultLalout, userNav and nav is work with router-link; logout is actually done

--- vue/src/store/index.js
+++ vue/src/store/index.js
@@ -5,13 +5,22 @@ const store = createStore({
     // npm run dev -- --port 5180
     state: {
         user:{
-            date: {},
-            token: null,
+            data: {
+                name: 'Martin German',
+                email: 'martin@example.com',
+                imageUrl: 'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+            },
+            token: 'null',
         }
     },
     getters: {},
     actions: {},
-    mutations: {},
+    mutations: {
+        logout(state){
+            state.user.token = null;
+            state.user.data = {};
+        }
+    },
     modules: {},
 })
 
