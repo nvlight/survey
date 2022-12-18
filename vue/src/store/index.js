3914f35ef9ed06125d642cac55d0663046ca411f
nvlight <lightstyle11@mail.ru>
2022-12-18 21:07:54 +0300

    adding dummy surveys data

--- vue/src/store/index.js
+++ vue/src/store/index.js
@@ -1,6 +1,363 @@
 import {createStore} from "vuex";
 import axiosClient from "../axios.js";
 
+const tmpSurveys = [
+    {
+        id: 100,
+        title: 'the Martin German youtube channel',
+        slug: 'the-Martin-German-youtube-channel',
+        status: 'draft',
+        image: "https://items.s1.citilink.ru/1809056_v01_b.jpg",
+        description: "My nama is Martin.<br> I am Web Developer with 9+ year of experience, free education com",
+        created_at: "2021-12-20 18:00:00",
+        updated_at: "2021-12-20 18:00:00",
+        expire_at: "2021-12-20 18:00:00",
+        questions: [
+            {
+                id: 1,
+                type: "select",
+                question: "From which country are you?",
+                description: null,
+                data: {
+                    options: [
+                        {
+                            uuid: "u3m89uc984ugvn43",
+                            text: 'russia',
+                        },
+                        {
+                            uuid: "hj3m487dchn3487h34cf",
+                            text: 'germany',
+                        },
+                        {
+                            uuid: "klsjdc893jn89c3df",
+                            text: 'italy',
+                        },
+                        {
+                            uuid: "938mcj9834jc9834j34c9",
+                            text: 'france',
+                        },
+                        {
+                            uuid: "u3m89uc984ugvn43",
+                            text: 'Georgia',
+                        },
+                    ],
+                },
+            },
+            {
+                id: 2,
+                type: "checkbox",
+                question: "Your favourite language?",
+                description: null,
+                data: {
+                    options: [
+                        {
+                            uuid: "389j4u9834un859fc43",
+                            text: 'php',
+                        },
+                        {
+                            uuid: "j89374nhujfc9834nb9834r",
+                            text: 'javacsript',
+                        },
+                        {
+                            uuid: "3984u89t4ybn45990rcun43",
+                            text: 'go',
+                        },
+                        {
+                            uuid: "9n843ufv9834nuyb5gv893",
+                            text: 'c++',
+                        },
+                        {
+                            uuid: "j98n3fcu89u3498vfcsdfg",
+                            text: 'python',
+                        },
+                    ],
+                },
+            },
+            {
+                id: 3,
+                type: "checkbox",
+                question: "The coolest PHP framework is?",
+                description: null,
+                data: {
+                    options: [
+                        {
+                            uuid: "kljshdfkslnuh87sdv7fhyn8",
+                            text: 'laravel',
+                        },
+                        {
+                            uuid: "a9l8mw4mjn98rc489",
+                            text: 'symfony',
+                        },
+                        {
+                            uuid: "slkdjf984jn89vr4",
+                            text: 'CodeIgniter',
+                        },
+                        {
+                            uuid: "aw984unr98a4uncr984",
+                            text: 'CakePHP',
+                        },
+                        {
+                            uuid: "c98aw4unjr984jn98rc",
+                            text: 'Yii',
+                        },
+                    ],
+                },
+            },
+            {
+                id: 4,
+                type: "radio",
+                question: "Best Javascript Frameworks for you is?",
+                description: null,
+                data: {
+                    options: [
+                        {
+                            uuid: "3j84nmtrc8934j89cr439",
+                            text: 'React JS',
+                        },
+                        {
+                            uuid: "1sj98cm4r98c4398vrjn8943",
+                            text: 'Vue JS',
+                        },
+                        {
+                            uuid: "3jmx8974r3984mxjnr9834j89r4",
+                            text: 'Angular',
+                        },
+                        {
+                            uuid: "897n4bty5jh89745utv984u598trcm4j589",
+                            text: 'Ember JS',
+                        },
+                        {
+                            uuid: "3q984rmjk9834jnvt89734nh7843",
+                            text: 'Svelte JS',
+                        },
+                    ],
+                },
+            },
+            {
+                id: 5,
+                type: "select",
+                question: "Top 20 Laravel Packages for 2022?",
+                description: null,
+                data: {
+                    options: [
+                        {
+                            uuid: "3984runmic9834um89cr43",
+                            text: 'Spatie',
+                        },
+                        {
+                            uuid: "3894nu5983c4un5c8934859",
+                            text: 'Laravel Debugbar',
+                        },
+                        {
+                            uuid: "4357n86hy4578h6ct784598mt4",
+                            text: 'Ignition',
+                        },
+                        {
+                            uuid: "3489n5uv39804u5v89345",
+                            text: 'Laravel Backup',
+                        },
+                        {
+                            uuid: "3498mc5mu9834muc589",
+                            text: 'No Captcha',
+                        },
+                    ],
+                },
+            },
+            {
+                id: 6,
+                type: "text",
+                question: "Your favourite youtube channel is?",
+                description: null,
+                data: {},
+            },
+            {
+                id: 7,
+                type: "textarea",
+                question: "What do you think about ASC youtube channel?",
+                description: "Write your honest opinion. Everything is anonymous.",
+                data: {},
+            },
+        ],
+    },
+    {
+        id: 101,
+        title: 'BESTWAY Mainframe',
+        slug: 'BESTWAY-Mainframe',
+        status: 'draft',
+        image: "https://items.s1.citilink.ru/1809061_v01_b.jpg",
+        description: "Самый лучший надувной матрас, правда стоит норм тоже!",
+        created_at: "2021-12-20 18:00:00",
+        updated_at: "2021-12-20 18:00:00",
+        expire_at: "2021-12-20 18:00:00",
+        questions: [
+            {
+                id: 1,
+                type: "select",
+                question: "From which country are you?",
+                description: null,
+                data: {
+                    options: [
+                        {
+                            uuid: "u3m89uc984ugvn43",
+                            text: 'russia',
+                        },
+                        {
+                            uuid: "hj3m487dchn3487h34cf",
+                            text: 'germany',
+                        },
+                        {
+                            uuid: "klsjdc893jn89c3df",
+                            text: 'italy',
+                        },
+                        {
+                            uuid: "938mcj9834jc9834j34c9",
+                            text: 'france',
+                        },
+                        {
+                            uuid: "u3m89uc984ugvn43",
+                            text: 'Georgia',
+                        },
+                    ],
+                },
+            },
+            {
+                id: 2,
+                type: "checkbox",
+                question: "Your favourite language?",
+                description: null,
+                data: {
+                    options: [
+                        {
+                            uuid: "389j4u9834un859fc43",
+                            text: 'php',
+                        },
+                        {
+                            uuid: "j89374nhujfc9834nb9834r",
+                            text: 'javacsript',
+                        },
+                        {
+                            uuid: "3984u89t4ybn45990rcun43",
+                            text: 'go',
+                        },
+                        {
+                            uuid: "9n843ufv9834nuyb5gv893",
+                            text: 'c++',
+                        },
+                        {
+                            uuid: "j98n3fcu89u3498vfcsdfg",
+                            text: 'python',
+                        },
+                    ],
+                },
+            },
+            {
+                id: 3,
+                type: "checkbox",
+                question: "The coolest PHP framework is?",
+                description: null,
+                data: {
+                    options: [
+                        {
+                            uuid: "kljshdfkslnuh87sdv7fhyn8",
+                            text: 'laravel',
+                        },
+                        {
+                            uuid: "a9l8mw4mjn98rc489",
+                            text: 'symfony',
+                        },
+                        {
+                            uuid: "slkdjf984jn89vr4",
+                            text: 'CodeIgniter',
+                        },
+                        {
+                            uuid: "aw984unr98a4uncr984",
+                            text: 'CakePHP',
+                        },
+                        {
+                            uuid: "c98aw4unjr984jn98rc",
+                            text: 'Yii',
+                        },
+                    ],
+                },
+            },
+            {
+                id: 4,
+                type: "radio",
+                question: "Best Javascript Frameworks for you is?",
+                description: null,
+                data: {
+                    options: [
+                        {
+                            uuid: "3j84nmtrc8934j89cr439",
+                            text: 'React JS',
+                        },
+                        {
+                            uuid: "1sj98cm4r98c4398vrjn8943",
+                            text: 'Vue JS',
+                        },
+                        {
+                            uuid: "3jmx8974r3984mxjnr9834j89r4",
+                            text: 'Angular',
+                        },
+                        {
+                            uuid: "897n4bty5jh89745utv984u598trcm4j589",
+                            text: 'Ember JS',
+                        },
+                        {
+                            uuid: "3q984rmjk9834jnvt89734nh7843",
+                            text: 'Svelte JS',
+                        },
+                    ],
+                },
+            },
+            {
+                id: 5,
+                type: "select",
+                question: "Top 20 Laravel Packages for 2022?",
+                description: null,
+                data: {
+                    options: [
+                        {
+                            uuid: "3984runmic9834um89cr43",
+                            text: 'Spatie',
+                        },
+                        {
+                            uuid: "3894nu5983c4un5c8934859",
+                            text: 'Laravel Debugbar',
+                        },
+                        {
+                            uuid: "4357n86hy4578h6ct784598mt4",
+                            text: 'Ignition',
+                        },
+                        {
+                            uuid: "3489n5uv39804u5v89345",
+                            text: 'Laravel Backup',
+                        },
+                        {
+                            uuid: "3498mc5mu9834muc589",
+                            text: 'No Captcha',
+                        },
+                    ],
+                },
+            },
+            {
+                id: 6,
+                type: "text",
+                question: "Your favourite youtube channel is?",
+                description: null,
+                data: {},
+            },
+            {
+                id: 7,
+                type: "textarea",
+                question: "What do you think about ASC youtube channel?",
+                description: "Write your honest opinion. Everything is anonymous.",
+                data: {},
+            },
+        ],
+    },
+];
+
 const store = createStore({
     // change vite port
     // npm run dev -- --port 5180
@@ -13,7 +370,8 @@ const store = createStore({
             // },
             data: {},
             token: sessionStorage.getItem("TOKEN"),
-        }
+        },
+        surveys: [...tmpSurveys],
     },
     getters: {},
     actions: {
