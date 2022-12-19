9beb547c252c332cbe1b5525fb86f8ab1329e547
nvlight <lightstyle11@mail.ru>
2022-12-19 23:08:25 +0300

    add image to survey is done!

--- app/Http/Controllers/SurveyController.php
+++ app/Http/Controllers/SurveyController.php
@@ -7,6 +7,9 @@ use App\Models\Survey;
 use App\Http\Requests\StoreSurveyRequest;
 use App\Http\Requests\UpdateSurveyRequest;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Str;
+use Mockery\Exception;
 
 class SurveyController extends Controller
 {
@@ -32,9 +35,17 @@ class SurveyController extends Controller
      */
     public function store(StoreSurveyRequest $request)
     {
-        $result = Survey::create($request->validated());
+        $data = $request->validated();
 
-        return new SurveyResource($result);
+        //
+        if (isset($data['image'])){
+            $relativePath = $this->saveImage($data['image']);
+            $data['image'] = $relativePath;
+        }
+
+        $survey = Survey::create($data);
+
+        return new SurveyResource($survey);
     }
 
     /**
@@ -66,9 +77,27 @@ class SurveyController extends Controller
      */
     public function update(UpdateSurveyRequest $request, Survey $survey)
     {
-        $result = $survey->update($request->validated());
+        $data = $request->validated();
 
-        return new SurveyResource($request);
+        //
+        if (isset($data['image'])){
+            $relativePath = $this->saveImage($data['image']);
+            $data['image'] = $relativePath;
+
+            // delete old image
+            if ($survey->image){
+                $absolutePath = public_path($survey->image);
+                try{
+                    File::delete($absolutePath);
+                }catch (Exception $e){
+                    throw new \Exception('file delete failing!');
+                }
+            }
+        }
+
+        $survey->update($data);
+
+        return new SurveyResource($survey);
     }
 
     /**
@@ -80,6 +109,7 @@ class SurveyController extends Controller
     public function destroy(Survey $survey, Request $request)
     {
         $user = $request->user();
+        // todo: поставить политики после!
         if ($user->id !== $survey->user_id){
             return abort(403, 'Unathorized action.');
         }
@@ -87,5 +117,40 @@ class SurveyController extends Controller
         $survey->delete();
 
         return response('', 204);
+    }
+
+    private function saveImage($image)
+    {
+        //
+        if (preg_match('/^data:image\/(\w+);base64,/', $image, $type)){
+            $image = substr($image, strpos($image, ',') + 1);
+            $type = strtolower($type[1]);
+            if (!in_array($type, ['jpg', 'jpeg', 'gif', 'png'])){
+                throw new \Exception('invalid image type');
+            }
+            $image = str_replace(' ', '+', $image);
+            $image = base64_decode($image);
+
+            if ($image === false){
+                throw new \Exception('base64_decode failed');
+            }
+        }else{
+            throw new \Exception('did not match data URI with image data');
+        }
+
+        $dir = 'images/';
+        $file = Str::random() . '.' . $type;
+        $absolutePath = public_path($dir);
+        $relativePath = $dir . $file;
+        if (!File::exists($absolutePath)){
+            File::makeDirectory($absolutePath, 0755, true);
+        }
+        try{
+            file_put_contents($relativePath, $image);
+        }catch (Exception $e){
+            throw new \Exception('file saving failing!');
+        }
+
+        return $relativePath;
     }
 }
